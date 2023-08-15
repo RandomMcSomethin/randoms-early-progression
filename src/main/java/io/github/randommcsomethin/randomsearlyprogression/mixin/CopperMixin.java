92125d7b91fc1f1c7eddccfc7ae838130540a82f
Aidan Gibson <hgcartoons@gmail.com>
2023-08-15 10:13:49 -0500

    updated to 1.1.0

--- src/main/java/io/github/randommcsomethin/randomsearlyprogression/mixin/CopperMixin.java
+++ src/main/java/io/github/randommcsomethin/randomsearlyprogression/mixin/CopperMixin.java
@@ -1,11 +1,16 @@
 package io.github.randommcsomethin.randomsearlyprogression.mixin;
 
+import com.mojang.datafixers.types.templates.Tag;
 import io.github.randommcsomethin.randomsearlyprogression.RandomsEarlyProgression;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.*;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -27,9 +32,19 @@ public abstract class CopperMixin extends LivingEntity {
     @Inject(at = @At("HEAD"), method = "canHarvest(Lnet/minecraft/block/BlockState;)Z", cancellable = true)
     private void canHarvest(BlockState state, CallbackInfoReturnable<Boolean> cir) {
         boolean usingFlintTool = this.inventory.getMainHandStack().isIn(RandomsEarlyProgression.FLINT_TOOLS);
-        if (usingFlintTool && this.inventory.getMainHandStack().isSuitableFor(state) && state.isIn(RandomsEarlyProgression.NEEDS_FLINT_TOOL)) {
+        if (usingFlintTool && canHarvestBlock(this.inventory.getMainHandStack().getItem(), state) && state.isIn(RandomsEarlyProgression.NEEDS_FLINT_TOOL)) {
             cir.setReturnValue(true);
         }
+    }
+
+    private boolean canHarvestBlock(Item tool, BlockState state) {
+        if (tool instanceof PickaxeItem && state.isIn(BlockTags.PICKAXE_MINEABLE)) {
+            return true;
+        } else if (tool instanceof AxeItem && state.isIn(BlockTags.AXE_MINEABLE)) {
+            return true;
+        } else if (tool instanceof ShovelItem && state.isIn(BlockTags.SHOVEL_MINEABLE)) {
+            return true;
+        } else return tool instanceof HoeItem && state.isIn(BlockTags.HOE_MINEABLE);
     }
 
 }
